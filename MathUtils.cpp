5fbacd91b3b752ec25bf527a02497b21b286bf6d
Tshepiso <makelanatshepiso2005@gmail.com>
2026-04-08 14:24:36 +0200

    Remove commented-out identity function header in Matrix4 class

--- MathUtils.cpp
+++ MathUtils.cpp
@@ -125,9 +125,6 @@ Matrix4 Matrix4::multiply(const Matrix4& other) const {
     return result;
 }
 
-// ---------------------------------------------------------------------------
-//  identity
-// ---------------------------------------------------------------------------
 Matrix4 Matrix4::identity() {
     // TODO: Create a Matrix4 and set m[0][0]=m[1][1]=m[2][2]=m[3][3]=1.
     Matrix4 mat;
